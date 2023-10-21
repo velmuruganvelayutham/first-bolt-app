2862c92da5f4d05a6304ec643113f6c29ba723ab
Velmurugan Velayutham <velayutham@juniper.net>
2023-10-21 12:21:17 +0530

    add listener

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
