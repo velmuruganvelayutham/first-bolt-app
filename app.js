8b8b956f258df44b4658d59c02f4686bf05ac04c
Velmurugan Velayutham <velayutham@juniper.net>
2023-11-28 14:18:59 +0530

    shortcut trigger_workflow

--- app.js
+++ app.js
@@ -7,6 +7,54 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+// The open_modal shortcut opens a plain old modal
+app.shortcut('trigger_workflow', async ({ shortcut, ack, client, logger }) => {
+  logger.info(`shortcut: ${JSON.stringify(shortcut)}`);
+  try {
+    // Acknowledge shortcut request
+    await ack();
+
+    // Call the views.open method using one of the built-in WebClients
+    const result = await client.views.open({
+      trigger_id: shortcut.trigger_id,
+      view: {
+        type: "modal",
+        title: {
+          type: "plain_text",
+          text: "My App"
+        },
+        close: {
+          type: "plain_text",
+          text: "Close"
+        },
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+            }
+          },
+          {
+            type: "context",
+            elements: [
+              {
+                type: "mrkdwn",
+                text: "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+              }
+            ]
+          }
+        ]
+      }
+    });
+
+    logger.info(JSON.stringify(result));
+  }
+  catch (error) {
+    logger.error(error);
+  }
+});
+
 app.action('button_click', async ({ body, ack, say }) => {
   // Acknowledge the action
   console.log('button_clicked');
@@ -224,7 +272,9 @@ app.message('hello', async ({ message, say }) => {
 
 
 /*
-userid to username mapping 
+fix the undefined in the conversation 
+userid to username mapping
+channelId - channelName - remove in the conversations
 show avaialble user at the top
 /rest API Endpoints with UI rendering for the conversations - messages.
 */
